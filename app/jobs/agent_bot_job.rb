b9908956d71ad4c1667e31dec229f4ed68a5f352
Amit Kumar <heroic@users.noreply.github.com>
2021-12-13 23:31:34 +0530

    Feature/rasa bot (#11)
    * Introduce rasa bot
    
    This modifies the agent bot to send data to rasa bot. The conversation is kept on pending status as long as the bot keeps replying. When the bot is not able to send a message back, we mark the conversation as open
    
    Co-authored-by: amit.kumar <amit.kumar@galaxycard.in>

--- app/jobs/agent_bot_job.rb
+++ app/jobs/agent_bot_job.rb
@@ -1,3 +1,36 @@
-class AgentBotJob < WebhookJob
+class AgentBotJob < ApplicationJob
   queue_as :bots
+
+  def perform(agent_bot_id, message_id)
+    return unless ENV['RASA_URL'].present?
+
+    message = Message.find(message_id)
+    response = Webhooks::Trigger.execute(ENV['RASA_URL'], {
+      sender: message.conversation_id,
+      message: message.content,
+      name: message.sender.name,
+      email: message.sender.email,
+      phone: message.sender.phone,
+      user_id: message.sender.custom_attributes.present? ? message.sender.custom_attributes['external_id'] : nil,
+    })
+
+    message.conversation.status = :open
+
+    return message.conversation.save unless response.code == 200
+
+    body = JSON.parse(response.body)
+
+    return message.conversation.save unless body[0].present? && body[0]['text'].present?
+
+    return message.conversation.save if body[0]['text'] == 'human_handoff'
+
+    agent_bot = AgentBot.find(agent_bot_id)
+    mb = Messages::MessageBuilder.new(agent_bot, message.conversation, {
+      content: body[0]['text'],
+    })
+    mb.perform
+
+    message.conversation.status = :pending
+    message.conversation.save
+  end
 end
--- app/listeners/agent_bot_listener.rb
+++ app/listeners/agent_bot_listener.rb
@@ -1,62 +1,58 @@
 class AgentBotListener < BaseListener
-  def conversation_resolved(event)
-    conversation = extract_conversation_and_account(event)[0]
-    inbox = conversation.inbox
-    return if inbox.agent_bot_inbox.blank?
-    return unless inbox.agent_bot_inbox.active?
+  # def conversation_resolved(event)
+  #   conversation = extract_conversation_and_account(event)[0]
+  #   inbox = conversation.inbox
+  #   return if inbox.agent_bot_inbox.blank?
+  #   return unless inbox.agent_bot_inbox.active?
 
-    agent_bot = inbox.agent_bot_inbox.agent_bot
+  #   agent_bot = inbox.agent_bot_inbox.agent_bot
 
-    payload = conversation.webhook_data.merge(event: __method__.to_s)
-    AgentBotJob.perform_later(agent_bot.outgoing_url, payload)
-  end
+  #   payload = conversation.webhook_data.merge(event: __method__.to_s)
+  #   AgentBotJob.perform_later(agent_bot.outgoing_url, payload)
+  # end
 
-  def conversation_opened(event)
-    conversation = extract_conversation_and_account(event)[0]
-    inbox = conversation.inbox
-    return if inbox.agent_bot_inbox.blank?
-    return unless inbox.agent_bot_inbox.active?
+  # def conversation_opened(event)
+  #   conversation = extract_conversation_and_account(event)[0]
+  #   inbox = conversation.inbox
+  #   return if inbox.agent_bot_inbox.blank?
+  #   return unless inbox.agent_bot_inbox.active?
 
-    agent_bot = inbox.agent_bot_inbox.agent_bot
+  #   agent_bot = inbox.agent_bot_inbox.agent_bot
 
-    payload = conversation.webhook_data.merge(event: __method__.to_s)
-    AgentBotJob.perform_later(agent_bot.outgoing_url, payload)
-  end
+  #   payload = conversation.webhook_data.merge(event: __method__.to_s)
+  #   AgentBotJob.perform_later(agent_bot.outgoing_url, payload)
+  # end
 
   def message_created(event)
     message = extract_message_and_account(event)[0]
-    inbox = message.inbox
-    return unless message.webhook_sendable? && inbox.agent_bot_inbox.present?
-    return unless inbox.agent_bot_inbox.active?
-
-    agent_bot = inbox.agent_bot_inbox.agent_bot
-
-    payload = message.webhook_data.merge(event: __method__.to_s)
-    AgentBotJob.perform_later(agent_bot.outgoing_url, payload)
+    return unless message.webhook_sendable?
+    return unless message.incoming?
+    
+    AgentBotJob.perform_later(message.inbox.agent_bot_inbox.agent_bot_id, message.id)
   end
 
-  def message_updated(event)
-    message = extract_message_and_account(event)[0]
-    inbox = message.inbox
-    return unless message.webhook_sendable? && inbox.agent_bot_inbox.present?
-    return unless inbox.agent_bot_inbox.active?
+  # def message_updated(event)
+  #   message = extract_message_and_account(event)[0]
+  #   inbox = message.inbox
+  #   return unless message.webhook_sendable? && inbox.agent_bot_inbox.present?
+  #   return unless inbox.agent_bot_inbox.active?
 
-    agent_bot = inbox.agent_bot_inbox.agent_bot
+  #   agent_bot = inbox.agent_bot_inbox.agent_bot
 
-    payload = message.webhook_data.merge(event: __method__.to_s)
-    AgentBotJob.perform_later(agent_bot.outgoing_url, payload)
-  end
+  #   payload = message.webhook_data.merge(event: __method__.to_s)
+  #   AgentBotJob.perform_later(agent_bot.outgoing_url, payload)
+  # end
 
-  def webwidget_triggered(event)
-    contact_inbox = event.data[:contact_inbox]
-    inbox = contact_inbox.inbox
-    return if inbox.agent_bot_inbox.blank?
-    return unless inbox.agent_bot_inbox.active?
+  # def webwidget_triggered(event)
+  #   contact_inbox = event.data[:contact_inbox]
+  #   inbox = contact_inbox.inbox
+  #   return if inbox.agent_bot_inbox.blank?
+  #   return unless inbox.agent_bot_inbox.active?
 
-    agent_bot = inbox.agent_bot_inbox.agent_bot
+  #   agent_bot = inbox.agent_bot_inbox.agent_bot
 
-    payload = contact_inbox.webhook_data.merge(event: __method__.to_s)
-    payload[:event_info] = event.data[:event_info]
-    AgentBotJob.perform_later(agent_bot.outgoing_url, payload)
-  end
+  #   payload = contact_inbox.webhook_data.merge(event: __method__.to_s)
+  #   payload[:event_info] = event.data[:event_info]
+  #   AgentBotJob.perform_later(agent_bot.outgoing_url, payload)
+  # end
 end
