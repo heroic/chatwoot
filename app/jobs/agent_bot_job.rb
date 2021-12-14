e431206e264bd720eefb863814c9fc53a3fd581d
amit.kumar <amit.kumar@galaxycard.in>
2021-12-14 17:44:20 +0530

    Fix bot job

--- app/jobs/agent_bot_job.rb
+++ app/jobs/agent_bot_job.rb
@@ -2,35 +2,33 @@ class AgentBotJob < ApplicationJob
   queue_as :bots
 
   def perform(agent_bot_id, message_id)
-    return unless ENV['RASA_URL'].present?
+    return if ENV['RASA_URL'].blank?
 
     message = Message.find(message_id)
-    response = Webhooks::Trigger.execute(ENV['RASA_URL'], {
-      sender: message.conversation_id,
-      message: message.content,
-      name: message.sender.name,
-      email: message.sender.email,
-      phone: message.sender.phone,
-      user_id: message.sender.custom_attributes.present? ? message.sender.custom_attributes['external_id'] : nil,
-    })
 
-    message.conversation.status = :open
+    bot_response = get_bot_response message
 
-    return message.conversation.save unless response.code == 200
-
-    body = JSON.parse(response.body)
-
-    return message.conversation.save unless body[0].present? && body[0]['text'].present?
-
-    return message.conversation.save if body[0]['text'] == 'human_handoff'
+    return message.conversation.update(status: :open) if bot_response.nil? || bot_response == 'human_handoff'
 
     agent_bot = AgentBot.find(agent_bot_id)
     mb = Messages::MessageBuilder.new(agent_bot, message.conversation, {
-      content: body[0]['text'],
-    })
+                                        content: bot_response
+                                      })
     mb.perform
+  end
 
-    message.conversation.status = :pending
-    message.conversation.save
+  private
+
+  def get_bot_response(message)
+    response = Webhooks::Trigger.execute(ENV['RASA_URL'], {
+                                           sender: message.conversation_id,
+                                           message: message.content
+                                         })
+
+    return unless response.code == 200
+
+    body = JSON.parse(response.body)
+
+    return body[0]['text'] if body[0].present? && body[0]['text'].present?
   end
 end
